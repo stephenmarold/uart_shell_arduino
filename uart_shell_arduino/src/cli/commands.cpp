73386c35af72558191632b8def2ca548c0be6de3
stephenmarold <stephen.marold@gmail.com>
2025-07-24 18:34:37 -0400

    parse set pin num high/low functionality added.

--- uart_shell_arduino/src/cli/commands.cpp
+++ uart_shell_arduino/src/cli/commands.cpp
@@ -68,6 +68,62 @@ void cmd_led(char* args) {
   }
 }
 
+void cmd_set(char* args){
+  if(!args || strlen(args) == 0){
+    Serial.println("Usage: <pin_num> <HIGH|LOW>");
+    return;
+  }
+
+  char* argsv[MAX_ARGS];
+  int argc = parse_args(args, argsv, MAX_ARGS);
+  if (argc != 3) {
+    Serial.println("Usage: <pin_num> <HIGH|LOW>");
+    return;
+  }
+
+  // Argument validation
+  if(strcmp(argsv[0], "pin") != 0){
+    Serial.println("Unsupported command. Try Again");
+    return;
+  }
+
+  int pin = atoi(argsv[1]);
+  if(pin > MAX_PINS || pin < 1){
+    Serial.println("Invalid pin number. Try Again");
+    return;
+  }
+
+  char* comm = argsv[2];
+  str_to_upper(comm);
+
+  // Parse comm
+  uint8_t comm_int;
+  if(strcmp(comm, "HIGH") == 0){
+    comm_int = HIGH;
+  } else if (strcmp(comm, "LOW") == 0){
+    comm_int = LOW;
+  } else {
+    Serial.println("Must set pin high or low. Try Again");
+    return;
+  }
+
+  //TODO: Refactor to configure pins with hardware, like in cli.cpp
+  Serial.println("Setting PinMode to OUTPUT");
+  pinMode(pin, OUTPUT);
+  Serial.print("Setting Pin ");
+  Serial.println(comm);
+  digitalWrite(pin, comm_int);
+
+}
+
+// Move to utils eventually. 
+void str_to_upper(char* str){
+  while(*str != '\0'){
+    *str = toupper((unsigned char)*str);
+    str++;
+  }
+}
+
 // command table
 CommandEntry commands[] = {
   {"help", cmd_help, "List available commands"},
@@ -75,5 +131,6 @@ CommandEntry commands[] = {
   {"version", cmd_version, "Show version information"},
   {"say_hello", cmd_say_hello, "Say hello to someone"},
   {"led", cmd_led, "Control the LED (on/off)"},
+  {"set", cmd_set, "Set's high or low state of a pin"},
   {nullptr, nullptr} // end marker
 };
--- uart_shell_arduino/src/cli/commands.h
+++ uart_shell_arduino/src/cli/commands.h
@@ -2,6 +2,7 @@
 #define COMMANDS_H
 
 #define MAX_ARGS 10
+#define MAX_PINS 13
 
 typedef void (*CommandFunction)(char* args);
 
@@ -21,5 +22,9 @@ void cmd_status(char* args);
 void cmd_version(char* args);
 void cmd_say_hello(char* args);
 void cmd_led(char* args);
+void cmd_set(char* args);
+
+//move later
+void str_to_upper(char* str);
 
 #endif
